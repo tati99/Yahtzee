120e5c92efc483c400fbdb0572226229edfcea3f
rafael rodrigues <rafael.rodrigues@solvimm.com>
2020-05-17 02:19:02 -0300

    modifying jogar_dados() function

--- Jogada.py
+++ Jogada.py
@@ -18,6 +18,25 @@ def jogar_Dados():
     global dados
     for i,k in enumerate(dados):
         dados[i] = random.randint(1,6)
+    '''
+    canvas = Canvas(width=400, height=250, bg='blue')   
+    
+    img = []
+    img.append(PhotoImage(file="dado_1.png"))
+    img.append(PhotoImage(file="dado_2.png"))
+    img.append(PhotoImage(file="dado_3.png"))
+    img.append(PhotoImage(file="dado_4.png"))
+    img.append(PhotoImage(file="dado_5.png"))
+    img.append(PhotoImage(file="dado_6.png"))
+    
+    
+    x=30
+    for el in dados:
+        canvas.create_image(x,100, anchor=NW, image=img[el-1])
+        x = x + 40
+    canvas.pack()
+    '''
+    
     return dados
 
 
