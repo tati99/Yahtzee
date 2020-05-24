4e9e5f5036bafab06a243d1af6960aa536c0a894
RafaelRodrigues23 <63918924+RafaelRodrigues23@users.noreply.github.com>
2020-05-23 22:49:07 -0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,5 +1,4 @@
 from tkinter import *
-
 __all__ = ['confCanvas', 'window']
 
 root = 0
@@ -13,8 +12,8 @@ def confCanvas():
     global root
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-    w = Canvas(root, width=screen_width, height=screen_height, bg='white')
-    button_quit = Button(root, padx=10, pady=10, text="Sair do Jogo", font='algerian', command=root.quit)
+    w = Canvas(root, width=800, height=800, bg='white')
+    button_quit = Button(root, text="Sair do Jogo", font='algerian', command=root.quit)
     w.pack()
     button_quit.pack()
     return w
